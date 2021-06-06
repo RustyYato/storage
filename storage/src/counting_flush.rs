f1a967bee7034c7afde08a028b06abf52d08a9a9
RustyYato <krishna.sd.2012@gmail.com>
2021-06-06 14:49:25 -0400

    inline more

--- storage/src/counting_flush.rs
+++ storage/src/counting_flush.rs
@@ -13,11 +13,12 @@ const THRESHOLD: u8 = 128;
 
 #[must_use = "storages don't do anything unless they are used"]
 pub struct CountingFlushStorage<S> {
-    storage: S,
+    pub storage: S,
     count: AtomicU8,
 }
 
 impl<S: Storage + Flush> CountingFlushStorage<S> {
+    #[inline]
     pub fn new(storage: S) -> Self {
         Self {
             storage,
@@ -62,11 +63,13 @@ impl<S: Storage + Flush> CountingFlushStorage<S> {
 }
 
 impl<S: Flush> Flush for CountingFlushStorage<S> {
+    #[inline]
     fn try_flush(&mut self) -> bool {
         *self.count.get_mut() = 0;
         self.storage.try_flush()
     }
 
+    #[inline]
     fn flush(&mut self) {
         *self.count.get_mut() = 0;
         self.storage.flush();
@@ -74,11 +77,13 @@ impl<S: Flush> Flush for CountingFlushStorage<S> {
 }
 
 impl<S: SharedFlush> SharedFlush for CountingFlushStorage<S> {
+    #[inline]
     fn try_shared_flush(&self) -> bool {
         self.count.store(0, Ordering::Relaxed);
         self.storage.try_shared_flush()
     }
 
+    #[inline]
     fn shared_flush(&self) {
         self.count.store(0, Ordering::Relaxed);
         self.storage.shared_flush();
@@ -86,23 +91,26 @@ impl<S: SharedFlush> SharedFlush for CountingFlushStorage<S> {
 }
 
 unsafe impl<S: OffsetHandle + Flush> OffsetHandle for CountingFlushStorage<S> {
+    #[inline]
     unsafe fn offset(&mut self, handle: Self::Handle, offset: isize) -> Self::Handle {
         self.storage.offset(handle, offset)
     }
 }
 
 unsafe impl<S: SharedOffsetHandle + SharedFlush> SharedOffsetHandle for CountingFlushStorage<S> {
+    #[inline]
     unsafe fn shared_offset(&self, handle: Self::Handle, offset: isize) -> Self::Handle {
         self.storage.shared_offset(handle, offset)
     }
 }
 
 unsafe impl<S: FromPtr + Flush> FromPtr for CountingFlushStorage<S> {
-    #[allow(clippy::cast_sign_loss)]
+    #[inline]
     unsafe fn from_ptr(&self, ptr: NonNull<u8>) -> Self::Handle { self.storage.from_ptr(ptr) }
 }
 
 unsafe impl<S: SharedGetMut + Flush> SharedGetMut for CountingFlushStorage<S> {
+    #[inline]
     unsafe fn shared_get_mut(&self, handle: Self::Handle) -> NonNull<u8> { self.storage.shared_get_mut(handle) }
 }
 
@@ -111,30 +119,37 @@ impl<S: MultiStorage + Flush> MultiStorage for CountingFlushStorage<S> {}
 unsafe impl<S: Storage + Flush> Storage for CountingFlushStorage<S> {
     type Handle = S::Handle;
 
+    #[inline]
     unsafe fn get(&self, handle: Self::Handle) -> NonNull<u8> { self.storage.get(handle) }
 
+    #[inline]
     unsafe fn get_mut(&mut self, handle: Self::Handle) -> NonNull<u8> { self.storage.get_mut(handle) }
 
+    #[inline]
     fn allocate_nonempty(&mut self, layout: NonEmptyLayout) -> Result<NonEmptyMemoryBlock<Self::Handle>, AllocErr> {
         self.count();
         self.storage.allocate_nonempty(layout)
     }
 
+    #[inline]
     unsafe fn deallocate_nonempty(&mut self, handle: Self::Handle, layout: NonEmptyLayout) {
         self.storage.deallocate_nonempty(handle, layout);
         self.count();
     }
 
+    #[inline]
     fn allocate(&mut self, layout: Layout) -> Result<MemoryBlock<Self::Handle>, AllocErr> {
         self.count();
         self.storage.allocate(layout)
     }
 
+    #[inline]
     unsafe fn deallocate(&mut self, handle: Self::Handle, layout: Layout) {
         self.storage.deallocate(handle, layout);
         self.count();
     }
 
+    #[inline]
     fn allocate_nonempty_zeroed(
         &mut self,
         layout: NonEmptyLayout,
@@ -143,6 +158,7 @@ unsafe impl<S: Storage + Flush> Storage for CountingFlushStorage<S> {
         self.storage.allocate_nonempty_zeroed(layout)
     }
 
+    #[inline]
     fn allocate_zeroed(&mut self, layout: Layout) -> Result<MemoryBlock<Self::Handle>, AllocErr> {
         self.count();
         self.storage.allocate_zeroed(layout)
@@ -150,6 +166,7 @@ unsafe impl<S: Storage + Flush> Storage for CountingFlushStorage<S> {
 }
 
 unsafe impl<S: ResizableStorage + Flush> ResizableStorage for CountingFlushStorage<S> {
+    #[inline]
     unsafe fn grow(
         &mut self,
         handle: Self::Handle,
@@ -161,6 +178,7 @@ unsafe impl<S: ResizableStorage + Flush> ResizableStorage for CountingFlushStora
         memory_block
     }
 
+    #[inline]
     unsafe fn grow_zeroed(
         &mut self,
         handle: Self::Handle,
@@ -172,6 +190,7 @@ unsafe impl<S: ResizableStorage + Flush> ResizableStorage for CountingFlushStora
         memory_block
     }
 
+    #[inline]
     unsafe fn shrink(
         &mut self,
         handle: Self::Handle,
@@ -185,26 +204,31 @@ unsafe impl<S: ResizableStorage + Flush> ResizableStorage for CountingFlushStora
 }
 
 unsafe impl<S: SharedStorage + SharedFlush> SharedStorage for CountingFlushStorage<S> {
+    #[inline]
     fn shared_allocate_nonempty(&self, layout: NonEmptyLayout) -> Result<NonEmptyMemoryBlock<Self::Handle>, AllocErr> {
         self.shared_count();
         self.storage.shared_allocate_nonempty(layout)
     }
 
+    #[inline]
     unsafe fn shared_deallocate_nonempty(&self, handle: Self::Handle, layout: NonEmptyLayout) {
         self.storage.shared_deallocate_nonempty(handle, layout);
         self.shared_count();
     }
 
+    #[inline]
     fn shared_allocate(&self, layout: Layout) -> Result<MemoryBlock<Self::Handle>, AllocErr> {
         self.shared_count();
         self.storage.shared_allocate(layout)
     }
 
+    #[inline]
     unsafe fn shared_deallocate(&self, handle: Self::Handle, layout: Layout) {
         self.storage.shared_deallocate(handle, layout);
         self.shared_count();
     }
 
+    #[inline]
     fn shared_allocate_nonempty_zeroed(
         &self,
         layout: NonEmptyLayout,
@@ -213,6 +237,7 @@ unsafe impl<S: SharedStorage + SharedFlush> SharedStorage for CountingFlushStora
         self.storage.shared_allocate_nonempty_zeroed(layout)
     }
 
+    #[inline]
     fn shared_allocate_zeroed(&self, layout: Layout) -> Result<MemoryBlock<Self::Handle>, AllocErr> {
         self.shared_count();
         self.storage.shared_allocate_zeroed(layout)
@@ -220,6 +245,7 @@ unsafe impl<S: SharedStorage + SharedFlush> SharedStorage for CountingFlushStora
 }
 
 unsafe impl<S: SharedResizableStorage + SharedFlush> SharedResizableStorage for CountingFlushStorage<S> {
+    #[inline]
     unsafe fn shared_grow(
         &self,
         handle: Self::Handle,
@@ -231,6 +257,7 @@ unsafe impl<S: SharedResizableStorage + SharedFlush> SharedResizableStorage for 
         memory_block
     }
 
+    #[inline]
     unsafe fn shared_grow_zeroed(
         &self,
         handle: Self::Handle,
@@ -242,6 +269,7 @@ unsafe impl<S: SharedResizableStorage + SharedFlush> SharedResizableStorage for 
         memory_block
     }
 
+    #[inline]
     unsafe fn shared_shrink(
         &self,
         handle: Self::Handle,
--- storage/src/flush_barrier.rs
+++ storage/src/flush_barrier.rs
@@ -11,39 +11,47 @@ pub struct FlushBarrier<S> {
 }
 
 impl<S> FlushBarrier<S> {
+    #[inline]
     pub const fn new(storage: S) -> Self { Self { storage } }
 }
 
 impl<S> Flush for FlushBarrier<S> {
+    #[inline]
     fn try_flush(&mut self) -> bool { true }
 
+    #[inline]
     fn flush(&mut self) {}
 }
 
 impl<S> SharedFlush for FlushBarrier<S> {
+    #[inline]
     fn try_shared_flush(&self) -> bool { true }
 
+    #[inline]
     fn shared_flush(&self) {}
 }
 
 unsafe impl<S: OffsetHandle> OffsetHandle for FlushBarrier<S> {
+    #[inline]
     unsafe fn offset(&mut self, handle: Self::Handle, offset: isize) -> Self::Handle {
         self.storage.offset(handle, offset)
     }
 }
 
 unsafe impl<S: SharedOffsetHandle> SharedOffsetHandle for FlushBarrier<S> {
+    #[inline]
     unsafe fn shared_offset(&self, handle: Self::Handle, offset: isize) -> Self::Handle {
         self.storage.shared_offset(handle, offset)
     }
 }
 
 unsafe impl<S: FromPtr> FromPtr for FlushBarrier<S> {
-    #[allow(clippy::cast_sign_loss)]
+    #[inline]
     unsafe fn from_ptr(&self, ptr: NonNull<u8>) -> Self::Handle { self.storage.from_ptr(ptr) }
 }
 
 unsafe impl<S: SharedGetMut> SharedGetMut for FlushBarrier<S> {
+    #[inline]
     unsafe fn shared_get_mut(&self, handle: Self::Handle) -> NonNull<u8> { self.storage.shared_get_mut(handle) }
 }
 
@@ -52,24 +60,31 @@ impl<S: MultiStorage> MultiStorage for FlushBarrier<S> {}
 unsafe impl<S: Storage> Storage for FlushBarrier<S> {
     type Handle = S::Handle;
 
+    #[inline]
     unsafe fn get(&self, handle: Self::Handle) -> NonNull<u8> { self.storage.get(handle) }
 
+    #[inline]
     unsafe fn get_mut(&mut self, handle: Self::Handle) -> NonNull<u8> { self.storage.get_mut(handle) }
 
+    #[inline]
     fn allocate_nonempty(&mut self, layout: NonEmptyLayout) -> Result<NonEmptyMemoryBlock<Self::Handle>, AllocErr> {
         self.storage.allocate_nonempty(layout)
     }
 
+    #[inline]
     unsafe fn deallocate_nonempty(&mut self, handle: Self::Handle, layout: NonEmptyLayout) {
         self.storage.deallocate_nonempty(handle, layout);
     }
 
+    #[inline]
     fn allocate(&mut self, layout: Layout) -> Result<MemoryBlock<Self::Handle>, AllocErr> {
         self.storage.allocate(layout)
     }
 
+    #[inline]
     unsafe fn deallocate(&mut self, handle: Self::Handle, layout: Layout) { self.storage.deallocate(handle, layout); }
 
+    #[inline]
     fn allocate_nonempty_zeroed(
         &mut self,
         layout: NonEmptyLayout,
@@ -77,12 +92,14 @@ unsafe impl<S: Storage> Storage for FlushBarrier<S> {
         self.storage.allocate_nonempty_zeroed(layout)
     }
 
+    #[inline]
     fn allocate_zeroed(&mut self, layout: Layout) -> Result<MemoryBlock<Self::Handle>, AllocErr> {
         self.storage.allocate_zeroed(layout)
     }
 }
 
 unsafe impl<S: ResizableStorage> ResizableStorage for FlushBarrier<S> {
+    #[inline]
     unsafe fn grow(
         &mut self,
         handle: Self::Handle,
@@ -92,6 +109,7 @@ unsafe impl<S: ResizableStorage> ResizableStorage for FlushBarrier<S> {
         self.storage.grow(handle, old, new)
     }
 
+    #[inline]
     unsafe fn grow_zeroed(
         &mut self,
         handle: Self::Handle,
@@ -101,6 +119,7 @@ unsafe impl<S: ResizableStorage> ResizableStorage for FlushBarrier<S> {
         self.storage.grow_zeroed(handle, old, new)
     }
 
+    #[inline]
     unsafe fn shrink(
         &mut self,
         handle: Self::Handle,
@@ -112,22 +131,27 @@ unsafe impl<S: ResizableStorage> ResizableStorage for FlushBarrier<S> {
 }
 
 unsafe impl<S: SharedStorage> SharedStorage for FlushBarrier<S> {
+    #[inline]
     fn shared_allocate_nonempty(&self, layout: NonEmptyLayout) -> Result<NonEmptyMemoryBlock<Self::Handle>, AllocErr> {
         self.storage.shared_allocate_nonempty(layout)
     }
 
+    #[inline]
     unsafe fn shared_deallocate_nonempty(&self, handle: Self::Handle, layout: NonEmptyLayout) {
         self.storage.shared_deallocate_nonempty(handle, layout);
     }
 
+    #[inline]
     fn shared_allocate(&self, layout: Layout) -> Result<MemoryBlock<Self::Handle>, AllocErr> {
         self.storage.shared_allocate(layout)
     }
 
+    #[inline]
     unsafe fn shared_deallocate(&self, handle: Self::Handle, layout: Layout) {
         self.storage.shared_deallocate(handle, layout);
     }
 
+    #[inline]
     fn shared_allocate_nonempty_zeroed(
         &self,
         layout: NonEmptyLayout,
@@ -135,12 +159,14 @@ unsafe impl<S: SharedStorage> SharedStorage for FlushBarrier<S> {
         self.storage.shared_allocate_nonempty_zeroed(layout)
     }
 
+    #[inline]
     fn shared_allocate_zeroed(&self, layout: Layout) -> Result<MemoryBlock<Self::Handle>, AllocErr> {
         self.storage.shared_allocate_zeroed(layout)
     }
 }
 
 unsafe impl<S: SharedResizableStorage> SharedResizableStorage for FlushBarrier<S> {
+    #[inline]
     unsafe fn shared_grow(
         &self,
         handle: Self::Handle,
@@ -150,6 +176,7 @@ unsafe impl<S: SharedResizableStorage> SharedResizableStorage for FlushBarrier<S
         self.storage.shared_grow(handle, old, new)
     }
 
+    #[inline]
     unsafe fn shared_grow_zeroed(
         &self,
         handle: Self::Handle,
@@ -159,6 +186,7 @@ unsafe impl<S: SharedResizableStorage> SharedResizableStorage for FlushBarrier<S
         self.storage.shared_grow_zeroed(handle, old, new)
     }
 
+    #[inline]
     unsafe fn shared_shrink(
         &self,
         handle: Self::Handle,
